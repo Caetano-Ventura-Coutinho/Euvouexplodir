adb72a28361baef5eb9a3670d52defc27b160a24
Caetano-Ventura-Coutinho <currytano@gmail.com>
2023-12-27 22:52:36 -0300

    adicionei muita coisa

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function nextPage(){
     window.location.href = "deucerto.html"
-}
+} 
+function moveButton(){  
+    var x = Math.random() * (window.innerWidth - document.getElementById('botaonao').offsetWidth) - 15;
+    var y = Math.random() * (window.innerHeight - document.getElementById('botaonao').offsetHeight) - 48;
+    document.getElementById('botaonao').style.left = `${x}px`;
+    document.getElementById('botaonao').style.top = `${y}px`;}
+
